2fb3d412a11607929580f0f7cb237df7f1490119
Paweł Dymon <dymonpawel@gmail.com>
2022-09-19 18:51:30 +0200

    add footer and clock to footer

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const iksButton = document.querySelector('.fa-x');
 const hamburgerMenu = document.querySelector('.hamburger__menu');
 
 
+
 const showMenu = () => {
     hamburgerMenu.style.display = "flex";    
     iksButton.style.display = 'block';
@@ -14,6 +15,25 @@ const closeMenu = () => {
     iksButton.style.display = 'none';
     hamburger.style.display = 'block';
 }
+
+const newDate = () => {
+    
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const sec = date.getSeconds();
+    const showTime = `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${sec < 10 ? "0" + sec : sec}`;
+
+    // time.innerHTML = showTime;
+    const time = document.querySelector('.time').innerText = showTime;
+    let t = setTimeout(() => { newDate() }, 1000);
+}
+
+newDate();
+
+
+
+
 
 hamburger.addEventListener('click', showMenu)
 iksButton.addEventListener('click', closeMenu)
